50927461b76f93d7d3324972c3fea86111654b8f
fahim <fahim.shoumik@gmail.com>
2024-03-03 17:39:26 +0600

    added console log for debug

--- src/client.ts
+++ src/client.ts
@@ -1,8 +1,10 @@
 import blocksFlexbox from 'grapesjs-blocks-flexbox'
 import basicBloc from 'grapesjs-blocks-basic'
-import {ClientConfig} from '@silexlabs/silex/dist/client/client'
-export default function (config: ClientConfig, options) {
-  console.log('client.ts', { config, options })
+//import {ClientConfig} from '@silexlabs/silex/dist/client/client'
+export default function (config) {
+  console.log('plugin init ...', { config })
   config.grapesJsConfig.plugins.push(blocksFlexbox)
+  console.log('flex bloc loaded')
   config.grapesJsConfig.plugins.push(basicBloc)
+  console.log('basic bloc loaded')
 }

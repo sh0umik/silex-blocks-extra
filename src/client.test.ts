64e06000a21ab97d0fc65a3ef72711602cf0e317
fahim <fahim.shoumik@gmail.com>
2024-03-03 12:30:31 +0600

    lint fix and lint added

--- src/client.test.ts
+++ src/client.test.ts
@@ -1,4 +1,4 @@
 import { expect, test } from '@jest/globals'
 test('client', () => {
-  expect("todo").toBeDefined();
-});
+  expect('todo').toBeDefined()
+})
--- src/client.ts
+++ src/client.ts
@@ -1,8 +1,8 @@
-import blocksFlexbox from 'grapesjs-blocks-flexbox';
-import basicBloc from 'grapesjs-blocks-basic';
-import {ClientConfig} from '@silexlabs/silex/dist/client/client';
+import blocksFlexbox from 'grapesjs-blocks-flexbox'
+import basicBloc from 'grapesjs-blocks-basic'
+import {ClientConfig} from '@silexlabs/silex/dist/client/client'
 export default function (config: ClientConfig, options) {
-  console.log('client.ts', { config, options });
-  config.grapesJsConfig.plugins.push(blocksFlexbox);
-  config.grapesJsConfig.plugins.push(basicBloc);
+  console.log('client.ts', { config, options })
+  config.grapesJsConfig.plugins.push(blocksFlexbox)
+  config.grapesJsConfig.plugins.push(basicBloc)
 }

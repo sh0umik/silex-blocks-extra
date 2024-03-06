5e28729ed396684e49a1c6b2b2b1d974fa5d4026
fahim <fahim.shoumik@gmail.com>
2024-03-07 00:55:06 +0600

    testing with plugin import

--- src/client.ts
+++ src/client.ts
@@ -1,17 +1,19 @@
-import blocksFlexbox from 'grapesjs-blocks-flexbox'
+import basicBlocs from 'grapesjs-blocks-basic'
 import { Plugin } from '@silexlabs/silex-plugins'
 import {ClientConfig} from "@silexlabs/silex/src/ts/client/config";
-import {BlockExtraOptions, PluginOptions} from "./interface";
-import loadBlocks from './blocks/flexbox';
+import {PluginOptions} from "./interface";
 import blocks from "./blocks";
 
 export default function (config : ClientConfig, options: PluginOptions) {
   console.log('plugin init client ...', { config, options })
 
-  console.log('passing label check : ' + options.label);
+  console.log('init basic blocs ...', { config, options })
+  config.grapesJsConfig.plugins.push(basicBlocs);
+
+  console.log('passing label check : ' + options.label)
   config.addPlugin([
     blocks as Plugin
-  ], options);
+  ], options)
 
-  return config;
+  return config
 }
